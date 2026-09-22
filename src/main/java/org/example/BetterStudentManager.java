6a5eb2f8969bd5c30d50cac1767b2c2657f17231
QTian157 <qtian31@gmail.com>
2026-09-22 18:44:52 -0500

    done

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -12,10 +12,18 @@ public class BetterStudentManager {
 
     public void addStudent(String name, double grade) {
         // This method should add a new student to the list of students
+        Student student = new Student(name, grade);
+        students.add(student);
     }
 
     public void removeStudent(String name) {
         // This method should remove the student with the given name from the list of students
+        for (Student student : students) {
+            if (student.name.equals(name)) {
+                students.remove(student);
+                break;
+            }
+        }
     }
 
     public String getStudentList() {
@@ -29,6 +37,14 @@ public class BetterStudentManager {
         // looping through the list of students
         //
         // Replace the following line with your implementation
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (Student student : students) {
+            result.append(student.name);
+            result.append(" ");
+            result.append(student.grade);
+            result.append("\n");
+        }
+
+        return result.toString();
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Student {
+    String name;
+    double grade;
+
+    public Student() {
+    }
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
